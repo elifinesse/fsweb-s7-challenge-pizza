16f987a437c78c7b5d23b3788094160fa1b35976
elifinesse <133097200+elifinesse@users.noreply.github.com>
2023-07-15 17:01:49 +0300

    some css styling for Pizza.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        Teknolojik Yemekler
-        <img src="../Assets/logo.svg" alt="Teknolojik Yemekler logo" />
-      </header>
       <Switch>
         <Route path="/" exact></Route>
         <Route path="/pizza">
--- src/Siparisler/Pizza.js
+++ src/Siparisler/Pizza.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+
 function Pizza() {
   const pizzaToppings = [
     "Pepperoni",
@@ -30,7 +31,6 @@ function Pizza() {
   }
   function orderInc() {
     setOrderNo(orderNo + 1);
-    console.log(toplam);
   }
   function orderDec() {
     orderNo > 1 ? setOrderNo(orderNo - 1) : setOrderNo(1);
@@ -40,103 +40,115 @@ function Pizza() {
     setToplam((paaPizza + secimler) * orderNo);
   }, [secimler, orderNo]);
   return (
-    <>
-      <header>
+    <div className="order-page">
+      <header className="order-header">
+        <img src="../../Assets/logo.svg" alt="Teknolojik Yemekler logo" />
         <nav>
-          <a href="#">Anasayfa</a>
-          <p>-</p>
-          <a href="#">Seçenekler</a>
-          <p>-</p>
-          <a href="#">
-            <b>Sipariş Oluştur</b>
-          </a>
+          <div>
+            <a href="#">Anasayfa</a> <span> - </span>
+          </div>
+          <div>
+            <a href="#"> Seçenekler</a> <span> - </span>
+          </div>
+          <div>
+            {" "}
+            <a href="#">
+              <b>Sipariş Oluştur</b>
+            </a>
+          </div>
         </nav>
       </header>
-      <div className="pizza-info">
-        <h3>Position Absolute Acı Pizza</h3>
-        <div className="pizza-numbers">
-          <h2>85.50₺</h2>
-          <p>4.9</p>
-          <p>(200)</p>
+      <div className="pizza-order">
+        <div className="pizza-info">
+          <h3>Position Absolute Acı Pizza</h3>
+          <div className="pizza-numbers">
+            <h2>85.50₺</h2>
+            <div className="reviews">
+              <p>4.9</p>
+              <p>(200)</p>
+            </div>
+          </div>
+          <p>
+            Frontend Dev olarak hala position: absolute kullanıyorsan bu çok acı
+            pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli
+            diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun
+            ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle
+            yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan
+            kökenli lezzetli bir yemektir. Küçük bir pizzaya bazen pizzetta
+            denir.
+          </p>
         </div>
-        <p>
-          Frontend Dev olarak hala position: absolute kullanıyorsan bu çok acı
-          pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli
-          diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun
-          ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak,
-          düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli
-          lezzetli bir yemektir. Küçük bir pizzaya bazen pizzetta denir.
-        </p>
-      </div>
-      <form id="pizza-form">
-        <div className="size">
-          <h3>
-            Boyut Seç<span className="required">*</span>
-          </h3>
-            <input type="radio" id="küçük" name="pizza_size" value="küçük" /> {" "}
-          <label htmlFor="küçük">Küçük</label>
-          <br></br>
-            <input
-            type="radio"
-            id="orta"
-            name="pizza_size"
-            value="orta"
-          />  <label htmlFor="orta">Orta</label>
-          <br></br>
-           <input
-            type="radio"
-            id="büyük"
-            name="pizza_size"
-            value="büyük"
-          />  <label htmlFor="büyük">Büyük</label>
-        </div>
-        <div className="dough">
-          <label>
+        <form id="pizza-form">
+          <div className="size">
             <h3>
-              Hamur Seç<span className="required">*</span>
+              Boyut Seç<span className="required">*</span>
             </h3>
-            <select name="pizza_dough" id="dough">
-              <option value="seç">Hamur Kalınlığı</option>
-              <option value="ince">İnce</option>
-              <option value="kalın">Kalın</option>
-            </select>
-          </label>
-        </div>
-        <div className="toppings"></div>
-        <h3>Ek Malzemeler</h3>
-        <p>En fazla 10 malzeme seçebilirsiniz. 5₺</p>
-        {pizzaToppings.map((malzeme) => (
-          <label onChange={addTopping}>
-            {" "}
-            <input type="checkbox" value={malzeme} name={malzeme} /> {malzeme}
-          </label>
-        ))}
-        <div className="order-note">
-          <label>
-            <h3>Sipariş Notu</h3>
-            <input type="text" />
-          </label>
-        </div>
-        <div className="order-info">
-          <div className="order-number">
-            <p onClick={orderDec}>-</p>
-            <p>{orderNo}</p>
-            <p onClick={orderInc}>+</p>
+              <input type="radio" id="küçük" name="pizza_size" value="küçük" /> {" "}
+            <label htmlFor="küçük">Küçük</label>
+            <br></br>
+              <input
+              type="radio"
+              id="orta"
+              name="pizza_size"
+              value="orta"
+            />  <label htmlFor="orta">Orta</label>
+            <br></br>
+             <input
+              type="radio"
+              id="büyük"
+              name="pizza_size"
+              value="büyük"
+            />  <label htmlFor="büyük">Büyük</label>
           </div>
-          <div className="order-calc">
-            <h3>Sipariş Toplamı</h3>
-            <div className="secimler">
-              <p>Seçimler</p>
-              <p>{secimler}</p>
+          <div className="dough">
+            <label>
+              <h3>
+                Hamur Seç<span className="required">*</span>
+              </h3>
+              <select name="pizza_dough" id="dough">
+                <option value="seç">Hamur Kalınlığı</option>
+                <option value="ince">İnce</option>
+                <option value="kalın">Kalın</option>
+              </select>
+            </label>
+          </div>
+          <div className="toppings"></div>
+          <h3>Ek Malzemeler</h3>
+          <p>En fazla 10 malzeme seçebilirsiniz. 5₺</p>
+          {pizzaToppings.map((malzeme) => (
+            <label onChange={addTopping}>
+              {" "}
+              <input type="checkbox" value={malzeme} name={malzeme} /> {malzeme}
+            </label>
+          ))}
+          <div className="order-note">
+            <label>
+              <h3>Sipariş Notu</h3>
+              <input type="text" />
+            </label>
+          </div>
+          <div className="order-info">
+            <div className="order-number">
+              <p onClick={orderDec}>-</p>
+              <p>{orderNo}</p>
+              <p onClick={orderInc}>+</p>
             </div>
-            <div className="toplam">
-              <p>Toplam</p>
-              <p>{toplam}</p>
+            <div className="order-calc">
+              <h3>Sipariş Toplamı</h3>
+              <div className="secimler">
+                <p>Seçimler</p>
+                <p>{secimler}</p>
+              </div>
+              <div className="toplam">
+                <p>Toplam</p>
+                <p>{toplam}</p>
+                <button type="submit">SİPARİŞ VER</button>
+              </div>
             </div>
           </div>
-        </div>
-      </form>
-    </>
+        </form>
+      </div>
+    </div>
   );
 }
 
